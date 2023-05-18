f326ec5a4757395cff49de2b947942dcf247e624
SoheilNamdar <soheilnamdar@yahoo.com>
2023-05-18 12:03:07 +0200

    Test done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,19 @@ import vehicle.Car;
 
 public class Main {
     public static void main(String[] args) {
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+
+        System.out.println("------------");
+
+        String brand = args[0];
+        String capacity = args[1];
+        String type = args[2];
+        Car car1 = new Car(brand, Integer.parseInt(capacity), type);
+        System.out.println(car1.getBrand());
+
         Car car = new Car();
         if (car.isItThatMovingForward())  {
             System.out.println("Moving forward.");
