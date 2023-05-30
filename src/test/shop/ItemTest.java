3871bf31c31609bb574376b54c68ab7a1602f802
SoheilNamdar <soheilnamdar@yahoo.com>
2023-05-30 09:39:54 +0200

    Test done

--- src/test/shop/ItemTest.java
+++ src/test/shop/ItemTest.java
@@ -1,10 +1,22 @@
 package shop;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ItemTest {
     @Test
     void should_check_if_item_names_are_not_null_or_empty() {
-        Item item = new Item();
+        ShoppingListService shoppingList = new ShoppingListServiceImpl();
+
+        List<Item> itemList = new LinkedList<>();
+        itemList.add(new Item("Banana", 5));
+        itemList.add(new Item("Apple", 3));
+
+
+        assertDoesNotThrow(() -> shoppingList.saveItems(itemList));
     }
 }
